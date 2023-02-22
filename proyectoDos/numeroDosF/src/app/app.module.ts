e4425a12329627dde019f73fe30b932cd613980a
unodosmangos <morellicj@yahoo.com>
2023-02-22 18:09:33 -0500

    avance 02.20

--- proyectoDos/numeroDosF/src/app/app.module.ts
+++ proyectoDos/numeroDosF/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-
-import { ReactiveFormsModule } from '@angular/forms';
 import { InventarioComponent } from './components/admin/inventario/inventario.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { MainAdminComponent } from './components/admin/main-admin/main-admin.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -19,11 +24,13 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
     RegisterComponent,
     InventarioComponent,
     NotfoundComponent,
+    MainAdminComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
